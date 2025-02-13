9724cdf4b144014d632fec0625c5094ec2311d7a
Adriano <adrianosouzaa733@gmail.com>
2025-02-13 14:04:58 -0300

    iniciando estudos em java com dados primitivos

---
+++ src/tiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,19 @@
+package tiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*
+        Dados primitivos =>
+        * Boolean / boolean
+        * Character / char
+        * integer / byte / short / int / long
+        * Float / float / double
+        * */
+
+        int idade = 19;
+        double altura = 1.65;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 999999L;
+    }
+}
