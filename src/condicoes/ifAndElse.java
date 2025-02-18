cba9319fb761da36b440df8021177c47b14069c1
Adriano <adrianosouzaa733@gmail.com>
2025-02-18 18:07:26 -0300

    condições em java

--- src/condicoes/ifAndElse.java
+++ src/condicoes/ifAndElse.java
@@ -1,4 +1,22 @@
 package condicoes;
 
 public class ifAndElse {
+    public static void main(String[] args) {
+
+        String nome = "Naruto Uzumaki";
+        String rank;
+
+        int idade = 10;
+        boolean hokage = false;
+        short numeroDeMissoes = 9;
+
+
+        if (numeroDeMissoes == 10 && idade > 15){
+            System.out.println("Rank: chunnin");
+        } else if (numeroDeMissoes >= 20){
+            System.out.println("Rank: Jounin");
+        } else {
+            System.out.println("Rank: Gennin");
+        }
+    }
 }
---
+++ src/tiposDeDados/Array2D.java
@@ -0,0 +1,4 @@
+package tiposDeDados;
+
+public class Array2D {
+}
