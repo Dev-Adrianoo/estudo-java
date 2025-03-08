4147abb6306c1471cdccc0fdcf2925f3702fed15
Adriano <adrianosouzaa733@gmail.com>
2025-03-07 22:54:30 -0300

    desafio 3 Main

--- src/desafio3/Main.java
+++ src/desafio3/Main.java
@@ -1,4 +1,22 @@
 package desafio3;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Uchiha Sasuke = new Uchiha();
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.idade = 19;
+        Sasuke.nivelDificuldade = "A";
+        Sasuke.Missao = "Salvar o Naruto";
+        Sasuke.Status = "Concluida!";
+        Sasuke.MostrarInformacoes();
+
+        Uzumaki Naruto = new Uzumaki();
+        Naruto.nome = "Naruto Uzumaki";
+        Naruto.idade = 18;
+        Naruto.nivelDificuldade = "A";
+        Naruto.Missao = "Ser Salvo pelo Sasuke";
+        Naruto.Status = "Concluida!";
+        Naruto.MostrarInformacoes();
+    }
 }
