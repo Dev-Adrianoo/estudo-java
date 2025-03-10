32b4e7901ca183827ed727fef4e97f64bc6ea630
Adriano <adrianosouzaa733@gmail.com>
2025-03-10 13:34:16 -0300

    Model

--- src/desafio3/Ninja.java
+++ src/desafio3/Ninja.java
@@ -1,4 +1,16 @@
 package desafio3;
 
+import java.util.Scanner;
+
 public class Ninja {
+
+    String nome;
+    int idade;
+    String nivelDificuldade;
+    String Missao;
+    String Status;
+
+    public void MostrarInformacoes(){
+        System.out.printf("Nome: %s\nIdade: %d\nDificuldade Missao: %s\nMissao: %s\nStatus Missao %s\n", nome, idade, nivelDificuldade, Missao, Status );
+    }
 }
